c23aabd73ac6b4170135669a96881ad10a388605
SatyamKumarCS <satyamkumarch15@gmail.com>
2025-11-12 16:42:23 +0530

    add starting port

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,13 @@ import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes";
 
+const PORT = process.env.PORT || 5001;
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoutes);
 
-app.listen(5001,()=>{
-    console.log("Server is running on port 5001");
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
 });
